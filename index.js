1b776134fa8e968fa1271c5a8293dd7af7f631d6
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-01-01 23:24:24 +0600

    adding mongoDB uri and dotenv

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 // middleware
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.ADMIN_NAME}:${process.env.ADMIN_PASSWORD}@cluster0.nr9ns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
 	res.send("Daily Shop is running");
 });
 app.listen(port, () => {
-	console.log("Server is running in ", port);
+	console.log("Server is running in ", port, uri);
 });
