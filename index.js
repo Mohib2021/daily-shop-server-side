5945e624ba786d574c05e760be2c9f6bff5133c2
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-01-02 01:48:02 +0600

    add express file-upload

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const app = express();
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
+const fileUpload = require("express-fileupload");
 
 // middleware
 app.use(cors());
+app.use(fileUpload());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 const uri = `mongodb+srv://${process.env.ADMIN_NAME}:${process.env.ADMIN_PASSWORD}@cluster0.nr9ns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
