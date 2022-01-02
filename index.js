ccffa490e7ec80fc82cf526811fe2721e524a8a8
Mohib2021 <mdmohibullasheikh@gmail.com>
2022-01-02 22:19:02 +0600

    add deleve single product apoi

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ const run = async () => {
 			res.send(result);
 		});
 
+		//Delete single product
+		app.delete("/products/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await productsCollections.deleteOne(query);
+			res.json(result);
+		});
+
 		// Upload product
 		app.post("/products", async (req, res) => {
 			const product = req.body;
